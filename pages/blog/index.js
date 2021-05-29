202dfd9d7f639ad5b49fff70aa78feaee37251ff
rick427 <rchard@yahoo.com>
2021-05-29 12:01:31 +0100

    added frontmatter

---
+++ pages/blog/index.js
@@ -0,0 +1,9 @@
+import Layout from "../../components/Layout";
+
+export default function BlogPage() {
+    return (
+        <Layout title="Our Blogs">
+            <h1>Blog page</h1>
+        </Layout>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,38 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 import Layout from '../components/Layout';
 
-export default function HomePage() {
+export default function HomePage({posts}) {
+  console.log(posts)
   return (
     <Layout>
-      <h1>Hello world</h1>
+      <h1>Latest Posts</h1>
     </Layout>
   )
+}
+
+export async function getStaticProps(){
+  const files = fs.readdirSync(path.join('posts'));
+
+  const posts = files.map(fileName => {
+    const slug = fileName.replace('.md', '');
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', fileName), 
+      'utf-8'
+    );
+
+    const {data: frontMatter} = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontMatter
+    }
+  })
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
